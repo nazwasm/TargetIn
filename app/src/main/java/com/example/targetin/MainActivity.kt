5e2171c17d8d8ab5ec13c77b8b516f560686e0a7
zalfaalfiah <alfiahzalfa21@gmail.com>
2025-07-03 09:30:47 +0700

    initial commit

---
+++ app/src/main/java/com/example/targetin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.targetin
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Delay 2 detik sebelum pindah ke halaman berikutnya
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, WishlistActivity::class.java))
+            finish()
+        }, 2000) // 2000ms = 2 detik
+    }
+}
---
+++ app/src/main/java/com/example/targetin/WishlistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.targetin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WishlistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wishlist)
+    }
+}
